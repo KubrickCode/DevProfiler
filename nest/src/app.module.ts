846f295d820790472206e6a44af20c7033ea1194
이승현 <kubrickcode@gmail.com>
2023-05-21 22:01:43 +0900

    fix: env 경로 수정

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -15,7 +15,7 @@ import { RedisModule } from './redis/redis.module';
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: './nest/.env',
     }),
     RedisModule,
   ],
